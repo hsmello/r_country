770488e50ba9e1c7a63f547c446e3e6c071d6cdc
Hugo Mello <hugosm6@hotmail.com>
2023-02-16 19:05:47 +0100

    add region, subregion, flag

--- app_v2.R
+++ app_v2.R
@@ -1,3 +1,4 @@
+# https://shiny.rstudio.com/images/shiny-cheatsheet.pdf
 # Load R packages
 library(shiny)
 library(shinythemes)
@@ -12,19 +13,26 @@ ui <- fluidPage(theme = shinytheme("darkly"),
                   tabPanel("Navbar 1",
                            
                            sidebarPanel(
-                             tags$h3("Input:"),
                              selectInput("combobox1", "Select a country", choices = data$name)
                              
                            ), # sidebarPanel
                            
                            mainPanel(
-                             h1("Header 1"),
+                             h2(textOutput("header_1")),
+                             
+                             uiOutput("flag_out"),
                              
                              h4("Capital"),
                              verbatimTextOutput("capital_out"),
                              
                              h4("Population"),
                              verbatimTextOutput("population_out"),
+                             
+                             h4("Region"),
+                             verbatimTextOutput("region_out"),
+                             
+                             h4("Sub-Region"),
+                             verbatimTextOutput("subregion_out")
                              
                            ) # mainPanel
                            
@@ -35,16 +43,18 @@ ui <- fluidPage(theme = shinytheme("darkly"),
                 ) # navbarPage
 ) # fluidPage
 
-
 # Define server function  
-
-
-#country_name <- data$name
 #country_capital <- data$capital
 #country_population <- data$population
 #country_population <- data$population
 
+country_name <- data$name
 server <- function(input, output) {
+  
+  output$header_1 <- renderText({
+    country_code = which(country_name == input$combobox1)
+    paste( data$name[country_code] )
+  })
 
   output$capital_out <- renderText({
     country_code = which(country_name == input$combobox1)
@@ -55,6 +65,23 @@ server <- function(input, output) {
     country_code = which(country_name == input$combobox1)
     paste( formatC(data$population[country_code], big.mark = "," ))
   })
+
+  output$region_out <- renderText({
+    country_code = which(country_name == input$combobox1)
+    paste( data$region[country_code])
+  })
+    
+  output$subregion_out <- renderText({
+    country_code = which(country_name == input$combobox1)
+    paste( data$subregion[country_code])
+  })
+
+  output$flag_out <- renderUI({
+    country_code = which(country_name == input$combobox1)
+    tags$img(src = data$flag[country_code], width = "30%")
+  })
+  
+
 } # server
 
 
